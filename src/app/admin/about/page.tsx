18f936ed8177ead92cfeaf7ab4bc9bd968420725
karthk <k4036119@gmail.com>
2025-08-08 16:45:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/about/page.tsx
+++ src/app/admin/about/page.tsx
@@ -1,4 +1,3 @@
-
 // This file is obsolete and has been moved to /src/app/admin/(authenticated)/about/page.tsx.
 // This content is here to prevent Next.js from treating it as a page and causing build errors.
 export default null;
