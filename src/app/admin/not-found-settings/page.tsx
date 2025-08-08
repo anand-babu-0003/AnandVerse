c2eb967b6250242717b4b9b37b9e2182704f9796
karthk <k4036119@gmail.com>
2025-08-08 16:43:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/not-found-settings/page.tsx
+++ src/app/admin/not-found-settings/page.tsx
@@ -1,3 +1,4 @@
+
 // This file is obsolete and has been moved to /src/app/admin/(authenticated)/not-found-settings/page.tsx.
 // This content is here to prevent Next.js from treating it as a page and causing build errors.
 export default null;
